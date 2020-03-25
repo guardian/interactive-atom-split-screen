6440382ae98a6bb416733c7e966bb211f1ad97b2
Duarte Carrilho da Graça <4561+duarte@users.noreply.github.com>
2020-03-25 09:49:36 +0100

    work in progress

--- atoms/default/client/js/main.js
+++ atoms/default/client/js/main.js
@@ -3,20 +3,23 @@ el.src = '<%= atomPath %>/app.js';
 document.body.appendChild(el);
 
 
+function tweakHeaderDom() {
+  let headerEl = document.querySelector('header.content__head--interactive');
+}
 
 setTimeout(() => {
-  if (window.resize) {  
+  if (window.resize) {
     const html = document.querySelector('html')
     const body = document.querySelector('body')
-  
+
     html.style.overflow = 'hidden'
     html.style.margin = '0px'
     html.style.padding = '0px'
-  
+
     body.style.overflow = 'hidden'
     body.style.margin = '0px'
     body.style.padding = '0px'
-  
-  window.resize()
+
+    window.resize()
   }
-},100)
+}, 100)
