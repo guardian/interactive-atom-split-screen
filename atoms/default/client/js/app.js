8b6b1b93e70d24af2436d00f128320a49ebb230e
Duarte Carrilho da Graça <4561+duarte@users.noreply.github.com>
2020-08-18 11:13:15 +0200

    initiate header quicker

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,12 +1,10 @@
 // if you want to import a module from shared/js then you can
 // just do e.g. import Scatter from "shared/js/scatter.js"
 
-
 checkReady();
 const checkReadyInterval = setInterval(function () {
   if (checkReady()) {
     clearInterval(checkReadyInterval);
-    addHeaderImage();
     createVisualPanel();
     syncVisualPanelScroll();
     customTypography();
@@ -140,34 +138,4 @@ function customTypography() {
       el.classList.add('hanging-quote');
     }
   })
-}
-
-
-// --- 5 ---- First showcase is header image
-function addHeaderImage() {
-  const headerEl = document.querySelector('header.content__head--interactive, .article .article__header');
-  const headerFigureEl = document.querySelector('.element-image.element--immersive, .element-image.element--showcase');
-  if (headerFigureEl) {
-    const headerFigureSrc = headerFigureEl.querySelector('img').getAttribute('src');
-    const srcHigh = higherResImg(headerFigureSrc);
-    headerEl.style.backgroundImage = `url(${srcHigh})`;
-    headerFigureEl.parentNode.removeChild(headerFigureEl);
-    if (headerFigureEl.classList.contains('element--immersive')) {
-      headerEl.classList.add('showcase-alt')
-    }
-  } else {
-    let standfirst = document.querySelector('.content__standfirst p');
-    if (standfirst) {
-      standfirst.innerText = 'Pssst, add an immersive/showcase image at the top of the article, so it can be used as a header photo! Immersive for white text, showcase for black';
-    }
-  }
-
-}
-
-function higherResImg(srcLow) {
-  let srcArray = srcLow.split('/')
-  srcArray.pop();
-  srcArray.push('2000.jpg');
-  let srcHigh = srcArray.join('/')
-  return srcHigh;
 }
--- atoms/default/client/js/main.js
+++ atoms/default/client/js/main.js
@@ -1,3 +1,33 @@
+addHeaderImage();
+function addHeaderImage() {
+  const headerEl = document.querySelector('header.content__head--interactive, .article .article__header');
+  const headerFigureEl = document.querySelector('.element-image.element--immersive, .element-image.element--showcase');
+  if (headerFigureEl) {
+    const headerFigureSrc = headerFigureEl.querySelector('img').getAttribute('src');
+    const srcHigh = higherResImg(headerFigureSrc);
+    headerEl.style.backgroundImage = `url(${srcHigh})`;
+    headerFigureEl.parentNode.removeChild(headerFigureEl);
+    if (headerFigureEl.classList.contains('element--immersive')) {
+      headerEl.classList.add('showcase-alt')
+    }
+  } else {
+    let standfirst = document.querySelector('.content__standfirst p');
+    if (standfirst) {
+      standfirst.innerText = 'Pssst, add an immersive/showcase image at the top of the article, so it can be used as a header photo! Immersive for white text, showcase for black';
+    }
+  }
+
+}
+
+function higherResImg(srcLow) {
+  let srcArray = srcLow.split('/')
+  srcArray.pop();
+  srcArray.push('2000.jpg');
+  let srcHigh = srcArray.join('/')
+  return srcHigh;
+}
+
+
 var el = document.createElement('script');
 el.src = '<%= atomPath %>/app.js';
 document.body.appendChild(el);
@@ -19,3 +49,4 @@ setTimeout(() => {
     window.resize()
   }
 }, 100)
+
